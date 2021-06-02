fa39d58ed15c1b1b473798e87d8fa51ac22a006c
Nate Pfau <natepfau@yahoo.com>
2021-06-01 21:19:40 -0500

    Save and Load tasks

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,9 +6,9 @@ var loadTasks = function()
 {
     $("#currentDay").text(currentTime.format("dddd, MMMM Do"));
 
-    tasks = localStorage.getItem("workDayTasks");
+    tasks = JSON.parse(localStorage.getItem("workDayTasks"));
 
-    if(!tasks);
+    if(!tasks)
     {
         tasks = [];
         //9am to 5pm
@@ -33,29 +33,41 @@ var loadTasks = function()
 
     for(var i = 0; i < tasks.length; i++)
     {
-        createTask(tasks[i].time, tasks[i].content);
+        createTask(tasks[i].time, tasks[i].content, i);
     }
 };
 
-var createTask = function(time, content)
+var createTask = function(time, content, i)
 {
-    var taskRow = $("<div>").addClass("row");
+    var taskRow = $("<div>").addClass("row").attr("data-index", i);
     var taskTime = $("<p>").addClass("col-1 hour").text(time);
     var taskHour = moment(time, "hA");
     var diff = currentTime.diff(taskHour, "hours");
+    var taskContent;
     if(diff >= 0)
     {
-        var taskContent = $("<p>").addClass("col-10 past").text(content);
+        taskContent = $("<textarea>").addClass("col-10 past").val(content);
     }
     else if(diff < -1)
     {
-        var taskContent = $("<p>").addClass("col-10 future").text(content);
+        taskContent = $("<textarea>").addClass("col-10 future").val(content);
     }
     else
     {
-        var taskContent = $("<p>").addClass("col-10 present").text(content);
+        taskContent = $("<textarea>").addClass("col-10 present").val(content);
     }
     var taskSave = $("<button>").addClass("col-1 saveBtn");
+    
+    taskSave.on("click", function()
+    {
+        var content = taskContent.val();
+        var index = taskRow.attr("data-index");
+
+        tasks[index].content = content;
+
+        localStorage.setItem("workDayTasks", JSON.stringify(tasks))
+    });
+
     var saveIcon = $("<i>").addClass("far fa-save");
     taskSave.append(saveIcon);
 
@@ -64,5 +76,6 @@ var createTask = function(time, content)
     taskRow.append(taskSave);
     $(".container").append(taskRow);
 }
+
 
 loadTasks();
